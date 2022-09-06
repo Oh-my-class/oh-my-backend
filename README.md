2a0961b359ee64eb073146f8336683572c8b7508
marvin <marvin131t@gmail.com>
2022-09-06 10:57:18 +0200

    Initial commid

---
+++ README.md
@@ -0,0 +1,5 @@
+# Oh my backend
+The RESTful API to the Oh My Class frontend [link to it here]
+
+## Setup
+1. Some steps
---
+++ src/main/java/com/ohmyclass/MyClassApplication.java
@@ -0,0 +1,12 @@
+package com.ohmyclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyClassApplication.class, args);
+	}
+}
