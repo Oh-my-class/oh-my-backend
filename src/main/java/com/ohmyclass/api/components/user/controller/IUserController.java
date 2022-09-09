c791eb40e8d937355a0118dc4d5b3b577bb30df9
Marvin <marvin131t@gmail.com>
2022-09-09 10:53:10 +0200

    Fixed mapping issues

--- src/main/java/com/ohmyclass/api/components/user/controller/IUserController.java
+++ src/main/java/com/ohmyclass/api/components/user/controller/IUserController.java
@@ -6,30 +6,30 @@ import com.ohmyclass.api.components.user.dto.out.UserOutDTO;
 import com.ohmyclass.api.util.ApiConst;
 import com.ohmyclass.api.util.communication.Response;
 import com.ohmyclass.util.other.Development;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(ApiConst.URL_ACCOUNT)
 public interface IUserController {
 
-	@PostMapping(ApiConst.GET)
+	@PostMapping(ApiConst.LOGIN)
 	Response<UserOutDTO> login(UserInDTO user);
 
-	@PostMapping(ApiConst.GET)
+	@PutMapping(ApiConst.REGISTER)
 	Response<UserOutDTO> register(UserInDTO user);
 
 	@Development
 	@PostMapping(ApiConst.GET)
 	Response<UserOutDTO> getUser(UserInDTO user);
 
-	@PostMapping(ApiConst.GET)
+	@PostMapping(ApiConst.UPDATE)
 	Response<UserOutDTO> updateUser(UserChangeInDTO user);
 
-	@PostMapping(ApiConst.GET)
+	@DeleteMapping(ApiConst.DELETE)
+	@Secured("Role_USER")
 	Response<Boolean> deleteUser(UserInDTO user);
 
-	@PostMapping(ApiConst.GET)
+	@PutMapping(ApiConst.PW_FORGOTTEN)
 	Response<UserOutDTO> passwordForgotten(UserInDTO user);
 }
--- src/main/java/com/ohmyclass/api/util/ApiConst.java
+++ src/main/java/com/ohmyclass/api/util/ApiConst.java
@@ -3,6 +3,15 @@ package com.ohmyclass.api.util;
 //TODO Do some sort of cfg
 public class ApiConst {
 
+	// ? Account
 	public static final String URL_ACCOUNT = "/account";
+	public static final String LOGIN = "/login";
+	public static final String REGISTER = "/register";
+	public static final String PW_FORGOTTEN = "/forgotten";
+
+	// ? General
 	public static final String GET = "/get";
+	public static final String INSERT = "/insert";
+	public static final String DELETE = "/delete";
+	public static final String UPDATE = "/update";
 }
