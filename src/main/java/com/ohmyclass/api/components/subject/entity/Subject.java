8fffe9ce91ba1a4b02dfc583b222b5e891d96373
Maurus Graf <maurus.graf@ergon.ch>
2022-09-13 11:05:47 +0200

    solve merge conflict

--- src/main/java/com/ohmyclass/api/components/subject/entity/Subject.java
+++ src/main/java/com/ohmyclass/api/components/subject/entity/Subject.java
@@ -4,14 +4,7 @@ import com.ohmyclass.api.components.user.entity.User;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.List;
 
 @Getter
