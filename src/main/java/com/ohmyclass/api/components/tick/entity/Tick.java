9edc26d6d45e836a1e3b131abe51f3b8bc238207
Maurus Graf <maurus.graf@ergon.ch>
2022-09-13 09:48:30 +0200

    implement entity structure for Tick

--- src/main/java/com/ohmyclass/api/components/tick/entity/Tick.java
+++ src/main/java/com/ohmyclass/api/components/tick/entity/Tick.java
@@ -1,3 +1,30 @@
 package com.ohmyclass.api.components.tick.entity;
 
-public class Tick {}
+import com.ohmyclass.api.components.task.entity.Task;
+import com.ohmyclass.api.components.user.entity.User;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Table(name = "tick")
+public class Tick {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false)
+	private Long id;
+
+	@OneToOne(fetch = FetchType.EAGER)
+	private Task task;
+
+	@OneToOne(fetch = FetchType.EAGER)
+	private User user;
+}
