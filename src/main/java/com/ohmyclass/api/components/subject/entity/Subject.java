fce706d373e0bf92c5c12131b03842f60e9e7811
Maurus Graf <maurus.graf@ergon.ch>
2022-09-13 10:31:31 +0200

    implement entity structure for Subject

--- src/main/java/com/ohmyclass/api/components/subject/entity/Subject.java
+++ src/main/java/com/ohmyclass/api/components/subject/entity/Subject.java
@@ -1,3 +1,35 @@
 package com.ohmyclass.api.components.subject.entity;
 
-public class Subject {}
+import com.ohmyclass.api.components.user.entity.User;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "subject")
+public class Subject {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false)
+	private Long id;
+
+	@Column(name = "name")
+	private String name;
+
+	@ManyToMany(fetch = FetchType.EAGER)
+	private List<User> teachers;
+
+	@Column(name = "color", length = 6)
+	private String color;
+}
--- src/main/java/com/ohmyclass/api/components/tick/entity/Tick.java
+++ src/main/java/com/ohmyclass/api/components/tick/entity/Tick.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -15,6 +16,7 @@ import javax.persistence.Table;
 
 @Getter
 @Setter
+@Entity
 @Table(name = "tick")
 public class Tick {
 	@Id
