1731a9ba9a4d41f20bfe3ffec9417872261b4a43
Maurus Graf <maurus.graf@ergon.ch>
2022-09-27 11:22:04 +0200

    implemented OutDTOs

--- src/main/java/com/ohmyclass/api/components/group/dto/out/GroupOutDTO.java
+++ src/main/java/com/ohmyclass/api/components/group/dto/out/GroupOutDTO.java
@@ -2,9 +2,13 @@ package com.ohmyclass.api.components.group.dto.out;
 
 import com.ohmyclass.api.components.subject.entity.Subject;
 import com.ohmyclass.api.components.task.entity.Task;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Set;
 
+@Getter
+@Setter
 public class GroupOutDTO {
 
 	private Long id;
--- src/main/java/com/ohmyclass/api/components/subject/dto/out/SubjectOutDTO.java
+++ src/main/java/com/ohmyclass/api/components/subject/dto/out/SubjectOutDTO.java
@@ -1,3 +1,11 @@
 package com.ohmyclass.api.components.subject.dto.out;
 
-public class SubjectOutDTO {}
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SubjectOutDTO {
+	private String name;
+	private String color;
+}
