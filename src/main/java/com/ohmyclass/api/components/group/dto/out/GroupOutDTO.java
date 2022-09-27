4faf5ad51b5b54e6c26631442b908c1c45281d97
Maurus Graf <maurus.graf@ergon.ch>
2022-09-27 10:19:00 +0200

    implemented GroupOutDTO

--- src/main/java/com/ohmyclass/api/components/group/dto/out/GroupOutDTO.java
+++ src/main/java/com/ohmyclass/api/components/group/dto/out/GroupOutDTO.java
@@ -1,3 +1,17 @@
 package com.ohmyclass.api.components.group.dto.out;
 
-public class GroupOutDTO {}
+import com.ohmyclass.api.components.subject.entity.Subject;
+import com.ohmyclass.api.components.task.entity.Task;
+
+import java.util.Set;
+
+public class GroupOutDTO {
+
+	private Long id;
+
+	private String name;
+
+	private Set<Subject> subjects;
+
+	private Set<Task> tasks;
+}
