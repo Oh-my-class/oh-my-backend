8c57ccaafda5b9b0fcd27b979a92e17373e3b838
Maurus Graf <maurus.graf@ergon.ch>
2022-11-08 08:10:50 +0100

    fix imports

--- src/main/java/com/ohmyclass/api/components/classmember/repository/IClassMemberRepository.java
+++ src/main/java/com/ohmyclass/api/components/classmember/repository/IClassMemberRepository.java
@@ -2,9 +2,8 @@ package com.ohmyclass.api.components.classmember.repository;
 
 import java.util.Optional;
 
+import com.ohmyclass.api.components.classmember.entity.ClassMember;
 import org.springframework.data.repository.CrudRepository;
-
-import com.ohmyclass.api.components.classmember.controller.impl.ClassMember;
 
 public interface IClassMemberRepository extends CrudRepository<ClassMember, Long> {
 
